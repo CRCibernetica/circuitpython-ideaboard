1250e6680f5a13309acabd6013f08ec7b421a623
Bentley Born <bentley@crcibernetica.com>
2022-11-18 08:09:45 -0600

    update examples

---
+++ examples/wifi_simple.py
@@ -0,0 +1,19 @@
+import socketpool
+import ssl
+import wifi
+import adafruit_requests as requests
+
+socket = socketpool.SocketPool(wifi.radio)
+https = requests.Session(socket, ssl.create_default_context())
+
+print("Connecting...")
+wifi.radio.connect("ssid", "password")
+print(f"Connected to Wifi!")
+
+URL = "http://api.open-notify.org/iss-now.json"
+
+data = https.get(URL).json()
+print(data)
+long = data["iss_position"]["longitude"]
+lat = data["iss_position"]["latitude"]
+print(f"The International Space Station is located at {lat}, {long}")
