e8dc84fb81f992f1f0823dde2992c842b1abaa57
Bentley Born <bentley@crcibernetica.com>
2023-06-03 20:54:53 -0700

    update map_range.py

--- examples/map_range.py
+++ examples/map_range.py
@@ -1,10 +1,9 @@
 import board
 from ideaboard import IdeaBoard
-from analogio import AnalogIn
 
 ib = IdeaBoard()
 
-pot = AnalogIn(board.IO33)
+pot = ib.AnalogIn(board.IO33)
 
 servo = ib.Servo(board.IO4)
 
