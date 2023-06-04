490c2a449ed4577c6ad2985774beacd1e9fa28ff
Bentley Born <bentley@crcibernetica.com>
2023-06-03 20:52:47 -0700

    updated analogout.py

--- examples/analogout.py
+++ examples/analogout.py
@@ -7,4 +7,4 @@ ib = IdeaBoard()
 # Note that there are only two pins on the IdeaBoard
 # that support AnalogOut: board.IO25 and board.IO26
 dac = ib.AnalogOut(board.IO26)
-dac.value = 65535 # 1.60V in pin board.IO26   
+dac.value = 32768 # 1.60V in pin board.IO26   
