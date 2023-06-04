ff622f4461faef47e1f3db504c39cf6c2a97fa6f
Bentley Born <bentley@crcibernetica.com>
2023-06-03 21:00:13 -0700

    added adafruit_io_test.py and secrets.py

---
+++ examples/secrets.py
@@ -0,0 +1,9 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'ssid',
+    'password' : 'password',
+    'aio_username' : 'adafruit_io_username',
+    'aio_key' : 'adafruit_io_key'
+}
